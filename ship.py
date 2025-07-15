b901096d0afa9e04b86f10251cbf7ab91962389e
Maksim Zabaluev <awesen561@gmail.com>
2025-07-15 15:53:38 -0400

    Added ability to fire lasers and having a limit to its amount, added laser sound.

--- ship.py
+++ ship.py
@@ -3,10 +3,10 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from alien_invasion import AlienInvasion
-
+    from arsenal import Arsenal
 
 class Ship:
-    def __init__(self, game: 'AlienInvasion'):
+    def __init__(self, game: 'AlienInvasion', arsenal: 'Arsenal'):
         self.game = game
         self.settings = game.settings
         self.screen = game.screen
@@ -22,18 +22,26 @@ class Ship:
         self.moving_right = False 
         self.moving_left = False
         self.x = float(self.rect.x)
+        self.arsenal  = arsenal
 
     def update(self):
          # updating the position of the ship
-         temp_speed = self.settings.ship_speed
-         if self.moving_right and self.rect.right < self.boundries.right: 
-              self.x += temp_speed
-         if self.moving_left and self.rect.left > self.boundries.left:
-              self.x -= temp_speed
+        self._update_ship_movement()
+        self.arsenal.update_arsenal() 
 
-         self.rect.x = self.x      
-         
+    def _update_ship_movement(self):
+        temp_speed = self.settings.ship_speed
+        if self.moving_right and self.rect.right < self.boundries.right: 
+             self.x += temp_speed
+        if self.moving_left and self.rect.left > self.boundries.left:
+             self.x -= temp_speed
 
-
+        self.rect.x = self.x 
+       
     def draw(self):
+            self.arsenal.draw()
             self.screen.blit(self.image, self.rect)
+
+    def fire(self):
+         return self.arsenal.fire_bullet()
+
