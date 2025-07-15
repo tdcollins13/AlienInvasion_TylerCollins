78022030dac41e856902dea1d89e27221f945fe1
Maksim Zabaluev <awesen561@gmail.com>
2025-07-15 08:41:09 -0400

    Added events for ship moving left and right, built in boundaries

--- ship.py
+++ ship.py
@@ -10,15 +10,30 @@ class Ship:
         self.game = game
         self.settings = game.settings
         self.screen = game.screen
-        self.screen_rect = self.screen.get_rect()
+        self.boundries = self.screen.get_rect()
 
         self.image = pygame.image.load(self.settings.ship_file)
         self.image = pygame.transform.scale(self.image, 
             (self.settings.ship_w, self.settings.ship_h)
-        )
+            )
 
         self.rect = self.image.get_rect()
-        self.rect.midbottom = self.screen_rect.midbottom
+        self.rect.midbottom = self.boundries.midbottom
+        self.moving_right = False 
+        self.moving_left = False
+        self.x = float(self.rect.x)
+
+    def update(self):
+         # updating the position of the ship
+         temp_speed = self.settings.ship_speed
+         if self.moving_right and self.rect.right < self.boundries.right: 
+              self.x += temp_speed
+         if self.moving_left and self.rect.left > self.boundries.left:
+              self.x -= temp_speed
+
+         self.rect.x = self.x      
+         
+
 
     def draw(self):
             self.screen.blit(self.image, self.rect)
